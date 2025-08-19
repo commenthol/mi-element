24d86e5db8df526fc13b5378ada748cb52ec0288
commenthol <commenthol@gmail.com>
2025-08-19 07:55:26 +0200

    fix(mi-html): use attach instead of reactive

--- packages/mi-html/src/index.js
+++ packages/mi-html/src/index.js
@@ -7,5 +7,5 @@ export {
 } from 'mi-signal'
 export * from 'uhtml/reactive'
 import { effect } from 'mi-signal'
-import { reactive } from 'uhtml/reactive'
-export const render = reactive(effect)
+import { attach } from 'uhtml/reactive'
+export const render = attach(effect)
